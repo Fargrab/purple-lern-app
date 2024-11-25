7aa784188d3acb611dc1a03559787686ae995d47
n.vyaskov <armnow911@yandex.ru>
2024-11-25 08:35:07 +0300

    First screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {Colors, FontSize, Gap} from "./shared/tokens";
+import Button from "./shared/Button/Button";
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>This is my first React Native App!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+          source={require('./assets/coffe_cup.png')}
+          style={styles.background}
+          imageStyle={styles.backgroundImage}
+      >
+        <View style={styles.contentStack}>
+          <View style={styles.textStack}>
+            <Text style={styles.headingText}>Одно из самых вкусных кофе в городе!</Text>
+            <Text style={styles.descriptionText}>Свежие зёрна, настоящая арабика и бережная обжарка</Text>
+          </View>
+          <Button text={"Начать"}/>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  backgroundImage: {
+    height: '70%',
+  },
+  contentStack: {
+    backgroundColor: Colors.black,
+    justifyContent: "flex-end",
+    gap: Gap.g24,
+    paddingHorizontal: 30,
+    paddingTop: 52,
+    paddingBottom: 24
+  },
+  textStack: {
+    alignItems: "center",
+    gap: Gap.g8
+  },
+  headingText: {
+    fontSize: FontSize.f34,
+    fontWeight: "600",
+    fontStyle: "normal",
+    textAlign: "center",
+    color: Colors.white
+  },
+  descriptionText: {
+    fontSize: FontSize.f14,
+    fontWeight: "400",
+    fontStyle: "normal",
+    textAlign: "center",
+    color: Colors.gray,
+    maxWidth: 300
   },
 });
